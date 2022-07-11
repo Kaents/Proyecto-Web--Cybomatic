f1f55133b3472e491e2de261edaa2a9db10402c7
Kaents <103613953+Kaents@users.noreply.github.com>
2022-07-11 03:06:55 -0400

    version 1.8.3

--- Estilos/validardatos.js
+++ Estilos/validardatos.js
@@ -1,4 +1,4 @@
-function validardatos() {
+function validarDatos() {
   nombre = document.getElementById("nombre").value
   apellido = document.getElementById("apellido").value
   email = document.getElementById("email").value
@@ -12,7 +12,7 @@ function validardatos() {
   } else {
     document.getElementById("errorNombre").style.display="none"
   }
-  if(apellido <5){
+  if(apellido.length <5){
     document.getElementById("errorApellido").style.display="block"
     errores++
   }else{
@@ -44,4 +44,25 @@ function validardatos() {
     return true    
   }
 
+}
+function validarDatos2() {
+  function validarDatos2 () {
+    nombre = document.getElementById("nombre").value
+    apellido = document.getElementById("apellido").value
+    rut = document.getElementById("rut").value
+    celular = document.getElementById("celular").value
+    direccion = document.getElementById("dirección").value
+    contraseña = document.getElementById("contraseña").value
+    confcontraseña = document.getElementById("pasw1").value
+    errores = 0
+    mensaje = "nombre:" + nombre + "apellido:" + apellido + "rut:" + rut + "celular:" + celular + "dirección:" + dirección + "contraseña:" + contraseña + "pasw1:" + pasw1
+    alert(mensaje)
+    if(nombre.length <3){
+      document.getElementById("errorNombre").style.display="block"
+      errores++
+    }else{
+      document.getElementById("errorNombre").style.display="none"
+    }
+    console.log(mensaje)
+  }
 }
